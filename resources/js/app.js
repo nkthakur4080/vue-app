a69f34c8dc22e730672d6c962a33829611cff960
nishant <nishant.thakur@adjetter.com>
2020-06-29 15:34:59 +0530

    css js database configure with npm install

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+import Buefy from 'buefy';
+
+Vue.use(Buefy);
